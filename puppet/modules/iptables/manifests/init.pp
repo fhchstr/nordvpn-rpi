b313ef2b49bb5d65909887a535a51e3b83329817
Fabien <fabien.hochstrasser@gmail.com>
2021-08-01 18:29:32 +0200

    Configure iptable rules to forward traffic from eth0 through tun0.
    Explicitly disabling IPv6 on tun0 isn't necessary, because the .all. or .default.
    sysctl rules already disable it for all interfaces.

--- puppet/modules/iptables/manifests/init.pp
+++ puppet/modules/iptables/manifests/init.pp
@@ -7,6 +7,24 @@ class iptables {
 
   class { ['firewall', 'iptables::pre', 'iptables::post']: }
 
+  # Set up forwarding through the VPN tunnel.
+  # sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+  firewall { '101 SNAT through tun0':
+    proto    => 'all',
+    chain    => 'POSTROUTING',
+    table    => 'nat',
+    outiface => 'tun0',
+    jump     => 'MASQUERADE',
+  }
+  # sudo iptables -A FORWARD -i eth0 -o tun0 -j ACCEPT
+  -> firewall { '102 forward eth0 through tun0':
+    proto    => 'all',
+    chain    => 'FORWARD',
+    iniface  => 'eth0',
+    outiface => 'tun0',
+    action   => 'accept'
+  }
+
   # If requested, purge unmanaged firewall rules and chains.
   if lookup('purge_other_iptable_rules') {
     resources {
@@ -22,6 +40,12 @@ class iptables {
       'INPUT:filter:IPv6':   ensure => present;
       'FORWARD:filter:IPv6': ensure => present;
       'OUTPUT:filter:IPv6':  ensure => present;
+      # Those chains are created by the forwarding rules.
+      'PREROUTING:nat:IPv4':  ensure => present;
+      'INPUT:nat:IPv4':       ensure => present;
+      'POSTROUTING:nat:IPv4': ensure => present;
+      'OUTPUT:nat:IPv4':      ensure => present;
+
     }
   }
 }
--- puppet/modules/nordvpn/manifests/init.pp
+++ puppet/modules/nordvpn/manifests/init.pp
@@ -16,7 +16,6 @@ class nordvpn (
     'net.ipv6.conf.all.disable_ipv6': value => 1;
     'net.ipv6.conf.default.disable_ipv6': value => 1;
     'net.ipv6.conf.lo.disable_ipv6': value => 1;
-    # 'net.ipv6.conf.tun0.disable_ipv6': value => 1; TODO: The tun0 interface doesn't exist yet. Puppet fails.
   }
   -> class { 'nordvpn::tools': }
 }
